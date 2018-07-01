a50059c0e42d7399aac61476bfa00d33df763306
Chris <chewning.cl@gmail.com>
2018-06-30 21:14:32 -0500

    put everything into a class

--- JS/app.js
+++ JS/app.js
@@ -1,92 +1,219 @@
+// window.onLoad = function() {
+
 
 const canvas = document.querySelector('canvas'); //DOM selector
 //jQuery selector needed instead
 var c = canvas.getContext('2d');
 
-// --------------------------- IMAGE VARIABLES ---------------------------------
+
+
+class Vehicle {
+  constructor(name, speed, direction, xPosition, yPosition, width, height){
+  this.name = name;
+  this.speed = speed;
+  this.direction = direction;
+  this.xPosition = xPosition;
+  this.yPosition = yPosition;
+  this.width = width;
+  this.height = height;
+}
+}
+
+  const sprite = new Vehicle('sprite', .5, 'right', 100, 40, 40, 40)
+
+
+  // tFly.reset = function(){
+  //  //set new random position
+  //  newX = Math.random() * this.cWidth;
+  //  newY = Math.random() * this.cHeight;
+  //  this.setPosition(newX, newY);
+  // } // end reset
+  //  tFly.reset();
+
+
+const reset = () => {
+  this.xPosition = 100;
+  this.yPosition = 40;
+}
+
+
+  //
+  // reset(xPosition, yPosition) {
+      // x.Position =
+      // y.Position =
+  //   }
+
+//WANT 10 vehicles
+
+//VEHICLES
+  const car = new Vehicle('car', 1.5, 'right', 0, 20, 50, 50);
+  const truck = new Vehicle('truck', .5, 'right', 0, 80, 50,50);
+  const bulldozer = new Vehicle('bulldozer', .5, 'right', 0, 100, 500, 500);
+  const van = new Vehicle('van', 1, 'left', 100, 40, 60, 60);
+  const tumbleweed = new Vehicle('tumbleweed', 1, 'left', 400, 60, 50, 50);
+
+
+
+
+
+console.log(Vehicle);
+
+
+// ------------------------ IMAGE VARIABLES ------------------------------
+
+//SPRITE IMAGE VARIABLE
+let spriteImage = new Image();
+spriteImage.src = 'https://cdn.iconscout.com/public/images/icon/premium/png-256/food-truck-car-transport-machine-movement-transportation-3486058b4a238844-256x256.png';
 
 // TUMBLEWEED IMAGE VARIABLE
 let tumbleweedImage = new Image();
 tumbleweedImage.src = 'https://i.imgur.com/jTqxjp1.png';
-// c.drawImage(tumbleweedImage, 0, 0);
-
 //CAR IMAGE VARIABLE
 let carImage = new Image();
-// carImage.src = images/car.png
 carImage.src = 'https://i.imgur.com/WYFaKX3.png';
-// c.drawImage(carImage, 0, 0);
-
 //TRUCK IMAGE VARIABLE
 let truckImage = new Image();
 truckImage.src = 'https://i.imgur.com/ML2VTl4.png';
-// c.drawImage(truckImage, 0, 0);
-
 //BULLDOZER IMAGE VARIABLE
 let bulldozerImage = new Image();
 bulldozerImage.src = 'https://i.imgur.com/NiY5TTh.png'
-
+//VAN IMAGE VARIABLE
 let vanImage = new Image();
-vanImage.src = 'https://i.imgur.com/uBO8u9w.png'
+vanImage.src = 'https://image.flaticon.com/icons/png/128/171/171255.png'
 
 
-//----------------------  Starting Points & Velocities  ------------------------
 
-let tumbleweed = 400;  //R - L
-let dtumbleweed = 1;
-let car = 0;  //L-R
-let dcar = 3; //velocity
-let truck = 0; //L-R
-let dtruck = 2;
-let bulldozer = 0; //L-R
-let dbulldozer = 2;
-let van = 400;  //R-L
-let dvan = 2.5;
-
-
-//----------------------  ANIMATE FUNCTION DRAWS THINGS  -----------------------
+//----------------------------  DRAW THE VEHICLES  -----------------------------
 
 const animate = () => {
   c.clearRect(0, 0, 1095, 468);
-  c.beginPath();
+  // c.beginPath(); //don't think you need.
   requestAnimationFrame(animate);
 
-  // x-axis, y-axis, width, height
-  c.drawImage(tumbleweedImage, tumbleweed, 60, 35, 35);
-  c.drawImage(carImage, car, 20, 35, 35); //x, y, how wide, how tall
-  c.drawImage(truckImage, truck, 80, 35, 35); //x, y, how wide, how tall
-  c.drawImage(bulldozerImage, bulldozer, 100, 35, 35);
-  c.drawImage(vanImage, van, 40, 35, 35)
+  // what image, x-starting pt., y-starting pt., width, height
+  c.drawImage(tumbleweedImage, tumbleweed.xPosition, tumbleweed.yPosition, tumbleweed.width, tumbleweed.height);
+  c.drawImage(carImage, car.xPosition, car.yPosition, car.width, car.height); //x, y, how wide, how tall
+  c.drawImage(truckImage, truck.xPosition, truck.yPosition, truck.width, truck.height); //x, y, how wide, how tall
+  c.drawImage(bulldozerImage, bulldozer.xPosition, bulldozer.yPosition, bulldozer.width, bulldozer.height);
+  c.drawImage(vanImage, van.xPosition, van.yPosition, van.width, van.height)
+  c.drawImage(spriteImage, sprite.xPosition, sprite.yPosition, sprite.width, sprite.height)
 
 
+  //------------------------  MAKE THE VEHICLES MOVE  --------------------------
+              //Remember: -= or += determine the direction.
 
-  // // for (var i = 0; i < 299; i++) {
-  // //   var x = Math.random() * window.innerWidth;
-  // //   var y = Math.random() * window.innerHeight;
-  // //
-  // //   const randomize = Math.floor(Math.random() * 3) + 1;
-  // //   if (randomize === 1){
-  // //     c.strokeStyle = 'blue';
-  // //   } else if (randomize === 2){
-  // //     c.strokeStyle = 'red';
-  // //   } else if (randomize === 3){
-  // //     c.strokeStyle = 'green';
-  // //   }
-  // //   console.log(randomize); //randomize function is working.
-  // //
-
-//-----------------------------  MAKE THINGS MOVE  -----------------------------
-
-  tumbleweed -= dtumbleweed;
-  car += dcar; //dx goes in a velocity one way or the other.
-  truck += dtruck;
-  bulldozer += dbulldozer;
-  van -= dvan;
+  tumbleweed.xPosition -= tumbleweed.speed;
+  car.xPosition += car.speed;
+  truck.xPosition += truck.speed;
+  bulldozer.xPosition += bulldozer.speed;
+  van.xPosition -= van.speed;
   console.log('hi');
 }
-animate(); //calls the function.
+
+//-----------------------------  MAKE SPRITE MOVE  -----------------------------
+
+function moveSprite(e) {
+  console.log('hi');
+  if (e.keyCode == 39) { //r
+    sprite.xPosition += 5;
+  }
+  if (e.keyCode == 37) { //l
+    sprite.xPosition -= 5;
+  }
+  if (e.keyCode == 38) { //up
+    sprite.yPosition -= 5;
+  }
+  if (e.keyCode == 40) { //down
+    sprite.yPosition += 5;
+    console.log(e.keycode);
+    canvas.width = canvas.width; //resets the canvas.
+  }
+  if (sprite.yPosition < -35) {
+    // alert('hi');
+    reset(sprite);
+    // sprite.reset(sprite.xPosition, sprite.yPosition); // Go back to the start
+    // score++; // Score increases;
+    // $("#score").text(score);
+  }
+  (console.log(sprite));
+}
+animate();
+document.onkeydown = moveSprite; //do not put () here.
+
+//REACHED SAFETY?
+
+
+// // Resets the player sprite back to the initial position
+//   reset(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+//
+//   // The player position resets when they reach the water
+//   update() {
+//     // Is the water reached?
+//
+//
+//     if (this.y <= 0) {
+//       this.reset(startPositionX, startPositionY); // Go back to the start
+//       score++; // Score increases;
+//       $("#score").text(score);
+//     }
+//   }
+
+
+// update() {
+//    // Is the water reached?
+//    if (this.y <= 0) {
+//      this.reset(startPositionX, startPositionY); // Go back to the start
+//      score++; // Score increases;
+//      $("#score").text(score);
+//    }
+// }
 
 
 //Could make them stop with these codes inside the function.
 // let x = canvas.width/2;
 // let y = canvas.height-30;
 // let z = canvas.width/3
+
+
+
+// COLLISION DETECTION
+
+
+//COLLISION CHECKER
+// const COLLIDED = 50; // Collision
+
+
+
+//SPEEDS FOR OBJECTS IN CLASS
+// const speeds = [300, 230, 400]; // Starting speeds
+
+
+//SCORE
+// let score = 0; // Starting score - 0
+//
+
+
+
+
+
+
+
+
+// Need to reset the player's direction. Turn them around. OR you can reset them back to the beginning, like below...
+
+// reset(x, y) {
+//    this.x = x;
+//    this.y = y;
+//  }
+//
+//  // The player position resets when they reach the water
+
+
+
+//to get a point
+// water -y axis is 450.
+
+//
